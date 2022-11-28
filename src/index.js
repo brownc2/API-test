7965ddc447164f362b6702b35a215b25687c0a31
Christopher <christopher2brown@hotmail.co.uk>
2022-11-28 12:51:45 +0000

    setup mongoose test database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 4000;
+
+mongoose.connect("mongodb://localhost/testdata", { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
