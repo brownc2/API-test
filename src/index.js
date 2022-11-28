5e237d0e6801bf999f93dc3fa15a952a75a1bd80
Christopher <christopher2brown@hotmail.co.uk>
2022-11-28 13:01:40 +0000

    Setup database in .env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 4000;
 
-mongoose.connect("mongodb://localhost/testdata", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
